bf5bb00547c5cd1dcc8270137a3bca12e96d1920
drago-do <dragodominguezfb@gmail.com>
2022-03-11 15:09:33 -0600

    Añadido boton flotante a medias, trabajarlo mejor con la base de datos ya implementada

---
+++ JS/productos.js
@@ -0,0 +1,23 @@
+listaDeProductos = [];
+
+//metodo que agrega un producto a la lista de productos
+function agregarProducto(codigoProducto) {
+  //busca el producto en la lista de productos de la base de datos
+  //!Se implementa despues
+  //Guardar los datos del producto en un objeto
+  listaDeProductos.push(codigoProducto);
+  //Muestra el boton de ir al carrito (boton flotante)
+  existenProductos();
+}
+
+
+//metodo que verifica si existen productos en la lista de productos
+function existenProductos() {
+  if (listaDeProductos.length > 0) {
+    //Muestra el boton de ir al carrito (boton flotante)
+    document.getElementById("btn-flotante").style.display = "block";
+  } else {
+    //Oculta el boton de ir al carrito (boton flotante)
+    document.getElementById("btn-flotante").style.display = "none";
+  }
+}
